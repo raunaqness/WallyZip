e9bbe1d9e458490c4ee4abc3d46126e245b975ee
gurraunaqsingh <raunaq.soni+gurraunaqsingh@gmail.com>
2017-04-23 19:35:44 +0530

    GG

--- app.py
+++ app.py
@@ -12,7 +12,29 @@ app = Flask(__name__)
 def home():
 	return render_template ('index.html')
 
+
 @app.route('/file-download/')
+def test():
+	filename = str(id) + ".jpg"
+	url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-169777.jpg"
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+	
+	# print(url)
+	r = requests.get(url, headers = headers, stream = True)
+	# print("Request successful")
+
+	# imgIO = StringIO.StringIO()
+	imgIO = io.BytesIO()
+	for chunk in r.iter_content(chunk_size=1024):
+		if chunk:
+			imgIO.write(chunk)
+
+	imgIO.seek(0)
+
+	return send_file(imgIO, 
+		attachment_filename=filename, 
+		as_attachment=True)
+
 def index():
 	wallhaven_random_url = "https://alpha.wallhaven.cc/random"
 	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
@@ -31,7 +53,7 @@ def index():
 	    return(download_image(image_id))
 	
 
-
+@app.route('/file-download/')
 def download_image(id):
 	filename = str(id) + ".jpg"
 	url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-" + str(id) + ".jpg"
