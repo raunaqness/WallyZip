3a4b3a6d9795b99250681e0d01b0a97ef9f5a5da
gurraunaqsingh <raunaq.soni+gurraunaqsingh@gmail.com>
2017-04-29 10:42:18 +0530

    GG Bros

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask, send_file, render_template
+from flask import Flask, send_file, render_template, make_response, send_from_directory
 from bs4 import BeautifulSoup
 import requests
 import os
 from PIL import Image
 import tempfile
 import io
+import shutil
+import zipfile
 
 app = Flask(__name__)
 
@@ -12,26 +14,63 @@ app = Flask(__name__)
 def home():
 	return render_template ('index.html')
 
-def download_image(id):
-	filename = str(id) + ".jpg"
-	url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"  + id + ".jpg"
+@app.route('/file-download/')
+def get_image_ids():
+	wallhaven_random_url = "https://alpha.wallhaven.cc/random"
 	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+
+	wallhaven_req = requests.get(wallhaven_random_url, headers=headers, verify=False)
+	content = wallhaven_req.text
+	soup = BeautifulSoup(content)
+	image_ids = []
+	image_id = ""
+
+	table = soup.find('section', attrs = {'class':'thumb-listing-page'})
+	uList = table.find('ul')
+
+	for item in uList.findAll('li'):
+	    image_id = str(item.figure['data-wallpaper-id'])
+	    image_ids.append(image_id)
+
+	return(download_images(image_ids))
 	
-	# print(url)
-	r = requests.get(url, headers = headers, stream = True)
-	# print("Request successful")
 
-	# imgIO = StringIO.StringIO()
-	imgIO = io.BytesIO()
-	for chunk in r.iter_content(chunk_size=1024):
-		if chunk:
-			imgIO.write(chunk)
+def download_images(ids):
 
-	imgIO.seek(0)
+	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+	image_folder = 'Images'
 
-	return send_file(imgIO, 
-		attachment_filename=filename, 
-		as_attachment=True)
+	if(os.path.isdir(image_folder)):
+		shutil.rmtree(image_folder, ignore_errors=True)
+
+	os.makedirs(image_folder)
+
+	for id in ids:
+		filename = str(id) + ".jpg"
+		url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"  + id + ".jpg"
+	
+		print(url)
+		response = requests.get(url, headers = headers, stream = True)
+		print("Request successful")
+
+		with open(os.path.join(image_folder,filename), 'wb') as out_file:
+			shutil.copyfileobj(response.raw, out_file)
+
+		# # imgIO = StringIO.StringIO()
+		# imgIO = io.BytesIO()
+		# for chunk in r.iter_content(chunk_size=1024):
+		# 	if chunk:
+		# 		imgIO.write(chunk)
+
+		# imgIO.seek(0)
+
+	print("Files downloaded succesfully")
+
+	return ("GGWP")
+
+	# return send_file(imgIO, 
+	# 	attachment_filename=filename, 
+	# 	as_attachment=True)
 
 @app.route('/app2')
 def download_image_2(id=29021):
@@ -47,53 +86,22 @@ def download_image_2(id=29021):
 	with open(os.path.join(image_folder,filename), 'wb') as out_file:
 		shutil.copyfileobj(response.raw, out_file)
 
+	# zipf = zipfile.ZipFile('images.zip', 'w', zipfile.ZIP_DEFLATED)
+	# for root, dirs, files in os.walk(image_folder):
+	# 	for file in files:
+	# 		zipf.write(os.path.join(root, file))
+	# zipf.close()
+
+	# zip out the image folder
 	zipf = zipfile.ZipFile('images.zip', 'w', zipfile.ZIP_DEFLATED)
 	for root, dirs, files in os.walk(image_folder):
-		for file in files:
-			zipf.write(os.path.join(root, file))
+	    for file in files:
+	        zipf.write(os.path.join(root, file))
 	zipf.close()
 
-	zipf2 = zipfile.ZipFile(os.path.abspath('images.zip'), 'r', zipfile.ZIP_DEFLATED)
-
-	# response = send_from_directory (os.path.abspath('images.zip'), 'images.zip', as_attachment=True,
- #                attachment_filename="images.zip",
- #                mimetype='application/zip')
-
-	# return response
-
-	return send_file(zipf2,
-		mimetype = 'application/zip',
-		as_attachment=True)
-
-	# if response.status_code == 200:
-	# 	return response(response.content,
-	# 		mimetype = 'application/zip',
-	# 		headers={'Content-Description' : 'attachment; filename=zones.zip'})
-
-	# zipf = zipfile.ZipFile('images.zip', 'r', zipfile.ZIP_DEFLATED)
-	# return response(zipf, 
-	# 	mimetype = 'application/zip',
-	# 	headers = {'Content-Description' : 'attachment; filename = zones.zip'})
+	return send_file('images.zip')
 
 
-@app.route('/file-download/')
-def index():
-	wallhaven_random_url = "https://alpha.wallhaven.cc/random"
-	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
-
-	wallhaven_req = requests.get(wallhaven_random_url, headers=headers, verify=False)
-	content = wallhaven_req.text
-	soup = BeautifulSoup(content)
-	ids = ""
-
-	table = soup.find('section', attrs = {'class':'thumb-listing-page'})
-	uList = table.find('ul')
-
-	for item in uList.findAll('li'):
-	    image_id = item.figure['data-wallpaper-id']
-	    ids = ids + " " + str((image_id))
-	    return(download_image(image_id))
-	
 
 if __name__ == '__main__':
 	app.run(debug=True)
--- app2.py
+++ app2.py
@@ -14,26 +14,33 @@ app = Flask(__name__)
 def home():
 	return render_template ('index.html')
 
-def download_image(id):
-	filename = str(id) + ".jpg"
-	url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"  + id + ".jpg"
+def download_images(ids):
+
 	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
+
+	for id in ids:
+		filename = str(id) + ".jpg"
+		url = "https://wallpapers.wallhaven.cc/wallpapers/full/wallhaven-"  + id + ".jpg"
 	
-	# print(url)
-	r = requests.get(url, headers = headers, stream = True)
-	# print("Request successful")
+		print(url)
+		r = requests.get(url, headers = headers, stream = True)
+		print("Request successful")
 
-	# imgIO = StringIO.StringIO()
-	imgIO = io.BytesIO()
-	for chunk in r.iter_content(chunk_size=1024):
-		if chunk:
-			imgIO.write(chunk)
+		# imgIO = StringIO.StringIO()
+		imgIO = io.BytesIO()
+		for chunk in r.iter_content(chunk_size=1024):
+			if chunk:
+				imgIO.write(chunk)
 
-	imgIO.seek(0)
+		imgIO.seek(0)
 
-	return send_file(imgIO, 
-		attachment_filename=filename, 
-		as_attachment=True)
+	print("Files downloaded succesfully")
+
+	return True
+
+	# return send_file(imgIO, 
+	# 	attachment_filename=filename, 
+	# 	as_attachment=True)
 
 @app.route('/app2')
 def download_image_2(id=29021):
@@ -49,51 +56,39 @@ def download_image_2(id=29021):
 	with open(os.path.join(image_folder,filename), 'wb') as out_file:
 		shutil.copyfileobj(response.raw, out_file)
 
+	# zipf = zipfile.ZipFile('images.zip', 'w', zipfile.ZIP_DEFLATED)
+	# for root, dirs, files in os.walk(image_folder):
+	# 	for file in files:
+	# 		zipf.write(os.path.join(root, file))
+	# zipf.close()
+
+	# zip out the image folder
 	zipf = zipfile.ZipFile('images.zip', 'w', zipfile.ZIP_DEFLATED)
 	for root, dirs, files in os.walk(image_folder):
-		for file in files:
-			zipf.write(os.path.join(root, file))
+	    for file in files:
+	        zipf.write(os.path.join(root, file))
 	zipf.close()
 
-	zipf2 = zipfile.ZipFile(os.path.abspath('images.zip'), 'r', zipfile.ZIP_DEFLATED)
-
-	# response = send_from_directory (os.path.abspath('images.zip'), 'images.zip', as_attachment=True,
- #                attachment_filename="images.zip",
- #                mimetype='application/zip')
-
-	# return response
-
-	return send_file(zipf2,
-		mimetype = 'application/zip',
-		as_attachment=True)
-
-	# if response.status_code == 200:
-	# 	return response(response.content,
-	# 		mimetype = 'application/zip',
-	# 		headers={'Content-Description' : 'attachment; filename=zones.zip'})
-
-	# zipf = zipfile.ZipFile('images.zip', 'r', zipfile.ZIP_DEFLATED)
-	# return response(zipf, 
-	# 	mimetype = 'application/zip',
-	# 	headers = {'Content-Description' : 'attachment; filename = zones.zip'})
+	return send_file('images.zip')
 
 @app.route('/file-download/')
-def index():
+def get_image_ids():
 	wallhaven_random_url = "https://alpha.wallhaven.cc/random"
 	headers = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"}
 
 	wallhaven_req = requests.get(wallhaven_random_url, headers=headers, verify=False)
 	content = wallhaven_req.text
 	soup = BeautifulSoup(content)
-	ids = ""
+	ids = []
 
 	table = soup.find('section', attrs = {'class':'thumb-listing-page'})
 	uList = table.find('ul')
 
 	for item in uList.findAll('li'):
 	    image_id = item.figure['data-wallpaper-id']
-	    ids = ids + " " + str((image_id))
-	    return(download_image(image_id))
+	    ids.append[str(image_id)]
+
+    return(download_image(ids))
 	
 
 if __name__ == '__main__':
