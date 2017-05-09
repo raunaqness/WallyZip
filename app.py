99c8bfef575ef2a7f78182a4463214f839732c24
gurraunaqsingh <raunaq.soni+gurraunaqsingh@gmail.com>
2017-05-09 14:22:13 +0530

    Made new index

--- app.py
+++ app.py
@@ -8,11 +8,16 @@ import io
 import shutil
 import zipfile
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="", static_folder="static")
+app.static_folder = 'static'
 
 @app.route('/')
 def home():
 	return render_template ('index.html')
+
+@app.route('/2')
+def home2():
+	return render_template ('index2.html')
 
 
 def get_image_ids():
